9b389af2427ddc3187f4c7f50737ab2408fd4ca3
pavel <mbpavel21@gmail.com>
2026-05-31 19:52:37 +0100

    fix: mode entrypoint.sh to /server

---
+++ server/entrypoint.sh
@@ -0,0 +1,9 @@
+# entrypoint.sh — à la racine du projet server
+#!/bin/sh
+set -e
+
+echo "🗄️  Migration Prisma..."
+node_modules/.bin/prisma migrate deploy
+
+echo "🚀 Démarrage PM2..."
+exec pm2-runtime start ecosystem.config.cjs
