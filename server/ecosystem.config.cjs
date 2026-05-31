3b0e4d6ae016f6007697a661de3a8e4431fe4c4b
pavel <mbpavel21@gmail.com>
2026-05-31 22:53:17 +0100

    fix copy prisma generatede folder to runner

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -2,21 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'pipolink-backend',
-      script: 'dist/server.js',
+      script: './dist/server.js',
       instances: 2,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
       },
-      error_file: './logs/pm2-error.log',
-      out_file: './logs/pm2-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       max_memory_restart: '500M',
-      restart_delay: 3000,
-      max_restarts: 10,
-      min_uptime: '10s',
-      listen_timeout: 5000,
-      kill_timeout: 5000,
+      watch: false,
     },
   ],
 };
