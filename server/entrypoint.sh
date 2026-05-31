4a634213d415471cd4283c1370a785c054017ec2
pavel <mbpavel21@gmail.com>
2026-05-31 20:32:36 +0100

    fix: update

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,8 +2,8 @@
 #!/bin/sh
 set -e
 
-echo "🗄️  Migration Prisma..."
-pnpm db:migrate-deploy
+# echo "🗄️  Migration Prisma..."
+# pnpm db:migrate-deploy
 
 echo "🚀 Démarrage PM2..."
 exec pm2-runtime start ecosystem.config.cjs
