0c506e40e4fc3af250e955a55a07245bb24c8bb5
pavel <mbpavel21@gmail.com>
2026-06-01 11:09:27 +0100

    fix: wb worker

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'pipolink-backend',
       script: './dist/server.js',
-      instances: 2,
+      instances: 1,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
