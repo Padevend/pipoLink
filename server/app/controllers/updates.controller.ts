ce0d69bf63ca85dbea72031d41157c08088f7aad
pavel <mbpavel21@gmail.com>
2026-06-02 12:46:09 +0100

    fix connextion

--- server/app/controllers/updates.controller.ts
+++ server/app/controllers/updates.controller.ts
@@ -4,21 +4,17 @@ import { ApiResponse } from "../helpers/api-response.js";
 export class UpdatesController {
   async getMetadata(c: HttpContext) {
     const metadata = {
-      version: "1.0.0",
+      version: "1.0.1",
       changelog: ["Amélioration de l'interface utilisateur", "Correction de bugs"],
-      isRequired: false,
-      minSdkVersion: "21",
-      severity: 'low',
+      isRequired: true,
+      minSdkVersion: "24",
+      severity: 'critical',
       type: 'manual',
       links: [
         {
           platform: 'android',
-          link: "https://play.google.com/store/apps/details?id=com.example.app",
-        },
-        {
-          platform: 'ios',
-          link: "https://apps.apple.com/app/id123456789",
-        },
+          link: "https://api-plink.lyrastudio.org/storage/release/V1.0.1/pipolink-app-release.apk",
+        }
       ]
     };
     return ApiResponse.success(c, metadata, "Métadonnées de mise à jour récupérées.");
