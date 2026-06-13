bfdacad763fc533fe163a579834712f397f9d9d7
pavel <mbpavel21@gmail.com>
2026-06-13 23:48:25 +0100

    update version

--- server/app/controllers/updates.controller.ts
+++ server/app/controllers/updates.controller.ts
@@ -4,16 +4,16 @@ import { ApiResponse } from "../helpers/api-response.js";
 export class UpdatesController {
   async getMetadata(c: HttpContext) {
     const metadata = {
-      version: "1.0.1",
+      version: "1.2.1",
       changelog: ["Amélioration de l'interface utilisateur", "Correction de bugs"],
-      isRequired: true,
+      isRequired: false,
       minSdkVersion: "24",
-      severity: 'critical',
-      type: 'manual',
+      severity: 'low',
+      type: 'auto',
       links: [
         {
           platform: 'android',
-          link: "https://api-plink.lyrastudio.org/storage/release/V1.0.1/pipolink-app-release.apk",
+          link: "https://api-plink.lyrastudio.org/storage/release/V1.2.1/pipolink-app-release.apk",
         }
       ]
     };
